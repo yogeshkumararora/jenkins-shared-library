c581cf73b0323eb57f120b1675de201cb4c557d8
yogeshkumararora <yogesh.arora@gmail.com>
2018-02-08 20:39:55 +0530

    Added logger in vars and removed from src

--- vars/logger.groovy
+++ vars/logger.groovy
@@ -5,5 +5,10 @@ def setLoggingLevel(String level) {
 }
 
 def info(String message) {
-    println("[INFO]:" + message)
+    println("[" + this.getCurrentTimestamp() +"] INFO:" + message)
+}
+
+def getCurrentTimestamp() {
+    def currentDate = new Date()
+    return currentDate.format("dd-MM-yyyy HH:mm:ss.SSS")
 }
